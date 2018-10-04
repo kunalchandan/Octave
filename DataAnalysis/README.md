9e21c41ca2abed7add46e44d1fdcc12a1f872ae1
Kunal Chandan <kunal_chandan@outlook.com>
2018-10-04 15:55:41 -0400

    Update Admissions Curves, added README

---
+++ DataAnalysis/README.md
@@ -0,0 +1,5 @@
+# University of Waterloo Admissions
+These are curves generated that are very similar to those seen on <https://theroadtoengineering.com/> or <https://profbillanderson.com/>
+Here is what I was able to generate.
+
+[embed]http://example.com/file.pdf[/embed]
--- DataAnalysis/cubicSpline.m
+++ DataAnalysis/cubicSpline.m
@@ -1,7 +1,34 @@
-x = [.85 .87 .92 .975 1.0];
-y = [0.0 .05 .45 .85  1.0];
-xx = .85:.001:1;
-yy = spline(x,y,xx);
-plot(x,y,'o',xx,yy)
+x = [.80 .87 .92 .975 1.0];
+yECE18 = [0.0 .05 .45 .85  .99];
+
+xENG = [.80 .87 .92 .975];
+yBMESE = [0.0 .02 .10 .50];
+yCEMMS = [0.0 .05 .45 .85];
+yACCEGMN = [0.0 .45 .85 .95];
+
+xx = .80:.001:1;
+yyECE18 = spline(x,yECE18,xx);
+yyBMSE = spline(xENG,yBMESE,xx);
+yyCEMMS = spline(xENG,yCEMMS,xx);
+yyACCEGMN = spline(xENG,yACCEGMN,xx);
+
+plot(
+% Plot the csplines
+  xx,yyBMSE,'--',"linewidth",3,
+  xx,yyCEMMS,'-.',"linewidth",3,
+  xx,yyACCEGMN,':',"linewidth",3,
+%  xx,yyECE18,"linewidth",3,
+
+% Plot the points
+  x,yECE18,'o',
+  xENG,yBMESE,'+',
+  xENG,yACCEGMN,'*',
+  xENG,yCEMMS,'x'
+);
+xlim([.80 1.0]);
+ylim([0.0 1.0]);
+xlabel ("Admission Average");
+ylabel ("Probability of Offer");
+title ("Probability of Admission Based on Top 6 Average, 2019 Season");
 
 % This is how i know i had a 2.75% chance of getting into UWaterlooooo for Computer Engineering
